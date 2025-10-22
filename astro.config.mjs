e50413470470cdd4bff2c3fc715cb1b997b8e913
45yposft <crsoedjh@gmail.com>
2025-10-22 12:10:49 +0100

    Actualizado dist
    El deploy no estaba cogiendo el fichero css

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,9 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   prefetch: {
+    prefetchAll: true,
     defaultStrategy: 'viewport',
   },
   site: 'https://45yposft.github.io',
+  base: '/prueba-practica-portfolio',
 });
